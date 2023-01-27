118e57f264e971f70e740a128e5a5702f341f6b7
kapysta000017 <kapysta000017@gmail.com>
2023-01-27 18:50:42 +0300

    price input

---
+++ src/components/Price.tsx
@@ -0,0 +1,47 @@
+import price from "./price.module.css"
+import { DefaultInputValues, InputValues } from "../pages/main/store/type"
+
+export default function Price({
+  setFormInputValues,
+  formInputValues,
+}: {
+  setFormInputValues: React.Dispatch<
+    React.SetStateAction<DefaultInputValues | InputValues>
+  >
+  formInputValues: DefaultInputValues
+}) {
+  return (
+    <div className={price.inner}>
+      <input
+        className={price.priceInput}
+        type="number"
+        placeholder="От"
+        onChange={(e) => {
+          setFormInputValues((state) => ({
+            ...state,
+            priceFrom: e.target.value,
+          }))
+          setFormInputValues((state) => ({
+            ...state,
+            differentPrice: Number(state.priceTo) - Number(state.priceFrom),
+          }))
+        }}
+        value={formInputValues.priceFrom}
+      />
+      <span className={price.pricesBetween}>-</span>
+      <input
+        className={price.priceInput}
+        type="number"
+        placeholder="До"
+        onChange={(e) => {
+          setFormInputValues((state) => ({ ...state, priceTo: e.target.value }))
+          setFormInputValues((state) => ({
+            ...state,
+            differentPrice: Number(state.priceTo) - Number(state.priceFrom),
+          }))
+        }}
+        value={formInputValues.priceTo}
+      />
+    </div>
+  )
+}
